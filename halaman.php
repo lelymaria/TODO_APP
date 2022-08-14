1e03334455ad446f7518cd4205124bd55eded656
lelymaria <lelymariakova@gmail.com>
2022-08-14 16:43:24 +0700

    Front-end

---
+++ halaman.php
@@ -0,0 +1,25 @@
+<?php
+include "layout/head.php";
+include "layout/nav.php";
+include "layout/side.php";
+?>
+<div class="container-fluid">
+	<?php
+	if (isset($_GET['page'])) {
+		switch ($_GET['page']) {
+			case "list_data":
+			include "page/list_data.php";
+			break;
+
+			default:
+			include "page/dashboard.php";
+			break;
+		}
+	} else {
+		include "page/dashboard.php";
+	}
+
+	?>
+</div>
+
+<?php include "layout/foot.php"; ?>
